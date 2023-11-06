6acfe38914ec142ada706d317b1a7e3949d74fd2
Roberto Zambrano <rzajr868@gmail.com>
2023-11-06 17:09:37 +0100

    .

--- app/src/main/java/com/example/persistencia/MainActivity.java
+++ app/src/main/java/com/example/persistencia/MainActivity.java
@@ -2,11 +2,19 @@ package com.example.persistencia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     String name,surname,phonenumber,email;
@@ -15,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button save = findViewById(R.id.savebuton);
-
+        Button layout_list = findViewById(R.id.seecontact);
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,11 +46,39 @@ public class MainActivity extends AppCompatActivity {
                     surname1.setText("");
                     phonenumber1.setText("");
                     mail1.setText("");
+
+                    String data = name + ";" + surname + ";" + phonenumber + ";" + email + "\n";
+
+
+                    // Obre l'arxiu per escriure
+                    try {
+                        String fileName = "contactes.txt";
+                        String content = "Contingut que vols escriure a l'arxiu.";
+
+                        FileOutputStream fos = openFileOutput(fileName, Context.MODE_APPEND);
+                        fos.write(data.getBytes());
+                        fos.close();
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+
                     Toast.makeText(MainActivity.this, "Saved.", Toast.LENGTH_SHORT).show();
                     
                 }
             }
         });
-
+    layout_list.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, Activity2.class);
+            startActivity(intent);
+        }
+    });
     }
+
+
+
 }
